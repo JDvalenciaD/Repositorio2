d6903da7b01578ce83b94b14e8b167cbcae99170
unknown <juancreator95@gmail.com>
2024-12-06 15:15:35 -0500

    Fundamentals done

---
+++ pruebas.js
@@ -0,0 +1,60 @@
+function mismonumero(str1,str2) {
+
+  if((str1 = 'juan'.length) === (str2 = 'luis'.length)) {
+    console.log('misma longitud');
+  }
+    else {   
+    console.log('diferente longitud');
+    }
+
+}
+
+
+
+
+esVocal('j');
+
+function saludoynombre(nombre) {
+
+  console.log("Hola " + nombre + "!"); 
+}
+
+saludoynombre('Andres');
+
+
+
+
+function Esvocal(letra) {
+
+  if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+
+    console.log('Es vocal');
+  }
+    
+  if(letra.length >= 2) {
+    console.log("Dato incorrecto");
+  }
+
+  else {
+    console.log('Dato incorrecto');
+  }   
+    
+}
+   Esvocal('a')
+   
+
+
+
+
+
+
+  
+ 
+ 
+
+ 
+
+
+
+
+
